c7165814a5cd305d945062119efc608aeef602dd
aleod72 <noappertfr@gmail.com>
2023-10-08 15:01:43 +0200

    edit

--- classes/hanoi.py
+++ classes/hanoi.py
@@ -6,18 +6,17 @@ from . import disques
 
 class Setup:
     def __init__(self, nbdisques):
-        self.tk = tk
-        self.root = self.tk.Tk()
-        self.root.resizable(False, False)
-
-        self.canvas = self.tk.Canvas(self.root, width=800, height=250)
-        self.canvas.pack()
 
         self.disques = [[], [], []]
         for taille in range(nbdisques):
             self.disques[0].append(disques.Setup(taille + 1))
         self.nbdisques = len(self.disques[0])
 
+        self.tk = tk
+        self.root = self.tk.Tk()
+        self.root.resizable(False, False)
+        self.canvas = self.tk.Canvas(self.root, width=800, height=250)
+        self.canvas.pack()
         self.text = tk.Label(
             self.root,
             text=f"                                                            Le jeu de Hanoï avec {nbdisques} disques...                                                            ",
@@ -27,6 +26,8 @@ class Setup:
         )
         self.text.place(relx=0.5, rely=1, y=-13, anchor="center")
         self.root.title(f"Hanoï : {self.nbdisques} disque.s")
+        self.buildCanvas()
+        self.root.update()
 
     def __str__(self):
         return str(self.disques)
@@ -77,28 +78,45 @@ class Setup:
             La fameuse fonction récursive qui permet de déplacer N disques
             """
             if n > self.nbdisques:
+                # n'arrivera jamais sauf démonstration
                 print(
                     f"Impossible de deplacer {n} disques, puisqu'il y en a {self.nbdisques}"
                 )
                 return
             if n == 0:
+                # "Hum.. pourquoi?"
                 return
             if n == 1:
+                # trop facile
                 bouge(depart, arrivee)
                 return
             if n == 2:
+                # ça va
                 deplace2pions(depart, arrivee, intermediaire)
                 return
             else:
+                # ben là tout dépend du n
+                # déplace les pions sur la tour intermédiaire (attention au cerveau les prochaines lignes sont dangereuses)
+                # en gros... ça va déplacer les pions sur une tour intermédiaire mais... 1/2 c'est la tour d'arrivée qui sert de tour intermédiaire
                 deplaceNpions(n - 1, depart, intermediaire, arrivee)
+                # bon une fois qu'il ne reste plus que le plus gros dans la première tour on le déplace dans la dernière tour (à ne pas sortir de son contexte)
                 bouge(depart, arrivee)
+                # et là... ça va [bla bla] mais... maintenant 1/2 la tour de départ sert de tour intermédiaire
                 deplaceNpions(n - 1, intermediaire, arrivee, depart)
+
+                # c'était la seule chose à vraiment expliquer de tout le code, merci
 
         deplaceNpions(
             self.nbdisques, 0, 2, 1
         )  # il y a surement mieux que ça mais voilà, tant que ça fonctionne c'est bon !
 
     def afficher(self):
+        self.buildCanvas()
+
+        time.sleep(0.5)
+        self.root.update()
+    
+    def buildCanvas(self):
         self.canvas.delete("all")
         self.canvas.create_rectangle(150, 200, 150, 50)
         self.canvas.create_rectangle(400, 200, 400, 50)
@@ -119,6 +137,3 @@ class Setup:
                     y + hauteur,
                     fill=self.disques[tour][-1 - disque].couleur,
                 )
-
-        self.root.update()
-        time.sleep(0.3)
